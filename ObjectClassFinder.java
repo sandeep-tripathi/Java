68aaa179d25a63afa1a4e04943e586236c439d8d
sandeep-tripathi <39844255+sandeep-tripathi@users.noreply.github.com>
2020-12-31 23:23:07 +0100

    Add files via upload

---
+++ ObjectClassFinder.java
@@ -0,0 +1,8 @@
+public class ObjectClassFinder{
+
+     public static void main(String []args){
+         Double number= 15.2;
+         System.out.println(number.getClass().getName());
+       
+     }
+}
